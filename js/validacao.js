ee00cdaf819ddd122b1734408a5ac0acc89893dc
Rafael Langoni <langoninaves@gmail.com>
2022-06-21 17:28:46 -0300

    validando CPF repetido

--- js/validacao.js
+++ js/validacao.js
@@ -36,12 +36,18 @@ const mensagensDeErro = {
     dataNascimento: {
         valueMissing: 'O campo data de nascimento não pode estar vazio.',
         customError: 'Você deve ter mais de 18 anos para se cadastrar'
+    },
+
+    cpf: {
+        valueMissing: 'O campo CPF não pode estar vazio.',
+        customError: 'O CPF informado é inválido.'
     }
 
 }
 
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+    dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCPF(input)
 }
 
 function mostraMensagemDeErro(tipoDeInput, input){
@@ -69,4 +75,39 @@ function maiorQue18(data){
     const dataAtual = new Date();
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
     return dataMais18 <= dataAtual;
+}
+
+function validaCPF(input){
+    const cpfFormatado = input.value.replace(/\D/g, '');
+    let mensagem = '';
+
+    if(!checaCPFRepetido(cpfFormatado)){
+        mensagem = 'O CPF informado é inválido.';
+    }
+
+    input.setCustomValidity(mensagem);
+}
+
+function checaCPFRepetido(cpf){
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    let cpfValido = true;
+
+    valoresRepetidos.forEach( valor => {
+        if(cpf == valor){
+            cpfValido = false;
+        }
+        return cpfValido;
+    })
 }
